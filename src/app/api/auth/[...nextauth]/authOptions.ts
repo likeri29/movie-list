d1ce24290a91d0219f51e6d91e03f60b64f364d3
likeri29 <s.pochtaruk29@gmail.com>
2024-12-18 17:10:57 +0100

    fix: reexported authOptions

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,51 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { AuthOptions } from "next-auth";
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!user) throw new Error("No user found with this email");
+
+        const isValid = await bcrypt.compare(
+          credentials?.password || "",
+          user.password
+        );
+
+        if (!isValid) throw new Error("Invalid password");
+
+        return user;
+      },
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+  callbacks: {
+    async session({ session, token }) {
+      (session.user as { id?: string }).id = token.sub as string;
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
+  },
+};
